dc27a93aceeea29416608092180adfc628e4c8a0
userisdabeh <120602326+userisdabeh@users.noreply.github.com>
2025-07-20 19:01:05 +0800

    create database connection script

---
+++ config/dbconn.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Dlsu1234!";
+$dbname = "rg_clothing";
+
+$dbconn = mysqli_connect($servername, $username, $password, $dbname);
+?>
